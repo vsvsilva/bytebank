56450f8086499ae822f348d2c47cc1b2c30185d8
Vitor <vitor_ovitor@outlook.com>
2022-02-08 14:29:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
+
+const cliente1 = new Cliente()
+const cliente2 = new Cliente()
+
+cliente1.nome = "Ricardo"
+cliente1.cpf = 11122233309
+cliente1.agencia = 1001
+cliente1.saldo = 0
+
+cliente2.nome = "Alice"
+cliente2.cpf = 12345678909
+cliente2.agencia = 1001
+cliente2.saldo = 0
+
+console.log(cliente1)
